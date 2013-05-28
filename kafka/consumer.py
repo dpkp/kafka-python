f4a326f490e347a28aa57b8b9d445c87972dc220
David Arthur <mumrah@gmail.com>
2013-05-28 10:35:40 -0400

    Closes #22
    Removed get_messages API, added test for get_pending

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -155,26 +155,6 @@ class SimpleConsumer(object):
                 assert resp.error == 0
             self.count_since_commit = 0
 
-    def get_messages(self, count=1):
-        """
-        Get the specified number of messages
-
-        count: maximum number of messages to be fetched
-        """
-        if not hasattr(self, '_iterator'):
-            self._iterator = iter(self)
-
-        msgs = []
-        while count > 0:
-            try:
-                msgs.append(self._iterator.next())
-                count -= 1
-            except StopIteration:
-                delattr(self, '_iterator')
-                break
-
-        return msgs
-
     def __iter__(self):
         """
         Create an iterate per partition. Iterate through them calling next() until they are
