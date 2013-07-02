ac9bad877ae2cc0dac883fc0f52b186fba20f5b0
Mahendra M <mahendra.m@gmail.com>
2013-07-02 14:08:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     name="kafka-python",
     version="0.8.1-1",
 
-    install_requires=["distribute", "tox", "gevent"],
-    tests_require=["tox", "gevent"],
+    install_requires=["distribute", "tox", "gevent", "kazoo"],
+    tests_require=["tox", "gevent", "kazoo"],
     cmdclass={"test": Tox},
 
     packages=["kafka"],
