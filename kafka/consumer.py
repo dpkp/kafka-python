6ba62e72e2a5a9330b81cc6ec127b7f03a78c121
Joe Crobak <joe@projectfla.com>
2013-10-21 20:07:43 +0000

    Enable absolute imports for modules using Queue.
    When running on Linux with code on a case-insensitive file system,
    imports of the `Queue` module fail because python resolves the
    wrong file (It is trying to use a relative import of `queue.py` in
    the kafka directory). This change forces absolute imports via PEP328.

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from collections import defaultdict
 from itertools import izip_longest, repeat
 import logging
--- kafka/producer.py
+++ kafka/producer.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from collections import defaultdict
 from itertools import cycle
 from multiprocessing import Queue, Process
