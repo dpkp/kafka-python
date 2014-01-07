adb7a63a5d033885fd7cfa972ba480dffe721813
Thomas Dimson <tdimson@gmail.com>
2014-01-06 17:37:41 -0800

    Check for socket status on read as well as send

--- kafka/conn.py
+++ kafka/conn.py
@@ -83,7 +83,6 @@ class KafkaConnection(local):
         """
         Get a response from Kafka
         """
-        log.debug("Reading response %d from Kafka" % request_id)
         # Read the size off of the header
         resp = self._read_bytes(4)
 
