6ee151427c144cc830eba954234f4f89f4529fe3
Mark Roberts <wizzat@gmail.com>
2014-04-07 19:44:40 -0700

    Remove test support for py26 since it's broken

---
+++ test/test_client.py
@@ -0,0 +1,27 @@
+import os
+import random
+import struct
+import unittest
+import kafka.client
+
+class ConnTest(unittest.TestCase):
+    def test_load_metadata_for_topics(self):
+        pass
+
+    def test_get_leader_for_partition(self):
+        pass
+
+    def test_get_leader_for_partition__no_leader(self):
+        pass
+
+    def test_get_conn_for_broker(self):
+        pass
+
+    def test_send_broker_unaware_request(self):
+        pass
+
+    def test_send_broker_unaware_request__no_brokers(self):
+        pass
+
+    def test_send_broker_unaware_request__all_brokers_down(self):
+        pass
