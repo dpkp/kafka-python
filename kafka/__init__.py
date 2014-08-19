a2d53ad71732f63fb631bc8c932fb73123281a5e
Tim Cherry <tcherry@livefyre.com>
2014-08-19 16:35:05 -0700

    pkg resource

--- kafka/__init__.py
+++ kafka/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'kafka'
 # Use setuptools to get version from setup.py
 import pkg_resources
-__version__ = pkg_resources.require('kafka-python')[0].version
+__version__ = pkg_resources.require('kafka-python-lf')[0].version
 __author__ = 'David Arthur'
 __license__ = 'Apache License 2.0'
 __copyright__ = 'Copyright 2012, David Arthur under Apache License, v2.0'
