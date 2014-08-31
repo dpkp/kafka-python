8cfc4c01d8b276402a289a56cc2e26f5fa419f0b
Dana Powers <dana.powers@rd.io>
2014-08-31 09:28:43 -0700

    Add trove classifiers for python 2.6, 2.7, and pypy to setup.py for PyPI

--- setup.py
+++ setup.py
@@ -48,6 +48,10 @@ is also supported for message sets.
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ]
 )
