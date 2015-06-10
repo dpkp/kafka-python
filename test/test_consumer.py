e60de943175c45d64602c0388b502b9763b34f27
Dana Powers <dana.powers@rd.io>
2015-06-10 09:38:24 -0700

    Split consumer test class between Simple and MultiProcess

--- test/test_consumer.py
+++ test/test_consumer.py
@@ -25,10 +25,11 @@ class TestMultiProcessConsumer(unittest.TestCase):
         client = MagicMock()
         partitions = (0,)
         with patch.object(MultiProcessConsumer, 'fetch_last_known_offsets') as fetch_last_known_offsets:
-            consumer = MultiProcessConsumer(client, 'testing-group', 'testing-topic', partitions=partitions)
+            MultiProcessConsumer(client, 'testing-group', 'testing-topic', partitions=partitions)
             self.assertEqual(fetch_last_known_offsets.call_args[0], (partitions,) )
         self.assertEqual(client.get_partition_ids_for_topic.call_count, 0) # pylint: disable=no-member
 
+class TestSimpleConsumer(unittest.TestCase):
     def test_simple_consumer_failed_payloads(self):
         client = MagicMock()
         consumer = SimpleConsumer(client, group=None,
