8ba168564c5ebae5e044cca20d4c7f30e17df3a0
Dana Powers <dana.powers@rd.io>
2015-11-28 19:40:07 +0800

    Fix __init__ legacy relative module import

--- kafka/protocol/__init__.py
+++ kafka/protocol/__init__.py
@@ -1,4 +1,4 @@
-from legacy import (
+from .legacy import (
     create_message, create_gzip_message,
     create_snappy_message, create_message_set,
     CODEC_NONE, CODEC_GZIP, CODEC_SNAPPY, ALL_CODECS,
