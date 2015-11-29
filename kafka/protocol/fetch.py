058567912e8d82c1da5e5ead9e30be532573a173
Dana Powers <dana.powers@rd.io>
2015-11-29 10:00:50 +0800

    Add simple BrokerConnection class; add request.RESPONSE_TYPE class vars

--- kafka/protocol/fetch.py
+++ kafka/protocol/fetch.py
@@ -3,9 +3,22 @@ from .struct import Struct
 from .types import Array, Int16, Int32, Int64, Schema, String
 
 
+class FetchResponse(Struct):
+    SCHEMA = Schema(
+        ('topics', Array(
+            ('topics', String('utf-8')),
+            ('partitions', Array(
+                ('partition', Int32),
+                ('error_code', Int16),
+                ('highwater_offset', Int64),
+                ('message_set', MessageSet)))))
+    )
+
+
 class FetchRequest(Struct):
     API_KEY = 1
     API_VERSION = 0
+    RESPONSE_TYPE = FetchResponse
     SCHEMA = Schema(
         ('replica_id', Int32),
         ('max_wait_time', Int32),
@@ -16,15 +29,4 @@ class FetchRequest(Struct):
                 ('partition', Int32),
                 ('offset', Int64),
                 ('max_bytes', Int32)))))
-    )
-
-class FetchResponse(Struct):
-    SCHEMA = Schema(
-        ('topics', Array(
-            ('topics', String('utf-8')),
-            ('partitions', Array(
-                ('partition', Int32),
-                ('error_code', Int16),
-                ('highwater_offset', Int64),
-                ('message_set', MessageSet)))))
     )
--- kafka/protocol/produce.py
+++ kafka/protocol/produce.py
@@ -3,9 +3,21 @@ from .struct import Struct
 from .types import Int8, Int16, Int32, Int64, Bytes, String, Array, Schema
 
 
+class ProduceResponse(Struct):
+    SCHEMA = Schema(
+        ('topics', Array(
+            ('topic', String('utf-8')),
+            ('partitions', Array(
+                ('partition', Int32),
+                ('error_code', Int16),
+                ('offset', Int64)))))
+    )
+
+
 class ProduceRequest(Struct):
     API_KEY = 0
     API_VERSION = 0
+    RESPONSE_TYPE = ProduceResponse
     SCHEMA = Schema(
         ('required_acks', Int16),
         ('timeout', Int32),
@@ -14,17 +26,4 @@ class ProduceRequest(Struct):
             ('partitions', Array(
                 ('partition', Int32),
                 ('messages', MessageSet)))))
-    )
-
-
-class ProduceResponse(Struct):
-    API_KEY = 0
-    API_VERSION = 0
-    SCHEMA = Schema(
-        ('topics', Array(
-            ('topic', String('utf-8')),
-            ('partitions', Array(
-                ('partition', Int32),
-                ('error_code', Int16),
-                ('offset', Int64)))))
     )
