64e9cebfa5e883464cfe76af0c3476ae542ac17b
Zack Dever <zdever@pandora.com>
2016-04-07 18:36:14 -0700

    Kafka metrics java port. No reporters or instrumentation.
    There is no straight translation for the JMX reporter into python,
    so I'll do something else in a separate commit.

---
+++ kafka/metrics/__init__.py
@@ -0,0 +1,12 @@
+from .compound_stat import NamedMeasurable
+from .kafka_metric import KafkaMetric
+from .measurable import AnonMeasurable
+from .metric_config import MetricConfig
+from .metric_name import MetricName
+from .metrics import Metrics
+from .quota import Quota
+
+__all__ = [
+    'AnonMeasurable', 'KafkaMetric', 'MetricConfig',
+    'MetricName', 'Metrics', 'NamedMeasurable', 'Quota'
+]
---
+++ kafka/metrics/stats/total.py
@@ -0,0 +1,13 @@
+from kafka.metrics.measurable_stat import AbstractMeasurableStat
+
+
+class Total(AbstractMeasurableStat):
+    """An un-windowed cumulative total maintained over all time."""
+    def __init__(self, value=0.0):
+        self._total = value
+
+    def record(self, config, value, now):
+        self._total += value
+
+    def measure(self, config, now):
+        return float(self._total)
