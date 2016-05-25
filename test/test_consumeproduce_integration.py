e06bd7818803cd5a44c2a45a6cf620390341b6c9
Alexander Sibiryakov <sixty-one@yandex.ru>
2016-05-25 12:29:06 +0200

    py3 friendly and trying to wait more

--- test/test_consumeproduce_integration.py
+++ test/test_consumeproduce_integration.py
@@ -5,9 +5,10 @@ from test.testutil import (
     KafkaIntegrationTestCase, kafka_versions, random_string
 )
 from kafka import (
-    SimpleConsumer, SimpleProducer
+    SimpleConsumer, SimpleProducer, SimpleClient
 )
 from kafka.protocol import CODEC_SNAPPY
+import six
 
 
 class TestConsumerIntegration(KafkaIntegrationTestCase):
@@ -21,6 +22,10 @@ class TestConsumerIntegration(KafkaIntegrationTestCase):
         cls.server = KafkaFixture.instance(0, cls.zk.host, cls.zk.port,
                                             zk_chroot=chroot)
 
+    def setUp(self):
+        super(TestConsumerIntegration, self).setUp()
+        self.client_producer = SimpleClient('%s:%d' % (self.server.host, self.server.port))
+
     @classmethod
     def tearDownClass(cls):
         if not os.environ.get('KAFKA_VERSION'):
@@ -29,10 +34,12 @@ class TestConsumerIntegration(KafkaIntegrationTestCase):
         cls.server.close()
         cls.zk.close()
 
+    @kafka_versions('>=0.8.1')
     def test_snappy(self):
         cons = SimpleConsumer(self.client, "group", self.topic)
-        prod = SimpleProducer(self.client, codec=CODEC_SNAPPY)
-        content = "hey-hey are you going to transfer me?"
+        prod = SimpleProducer(self.client_producer, codec=CODEC_SNAPPY)
+        content = six.b("hey-hey are you going to transfer me?")
         prod.send_messages(self.topic, content)
-        msg = cons.get_message()
+        msg = cons.get_message(timeout=1.0)
         assert msg.message.value == content
+
