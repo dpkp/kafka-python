1e532c674067e2399fa014151a2e8e49caa4fe58
steve8918 <steve8918@gmail.com>
2016-06-17 10:44:06 -0700

    tweak spelling mistake (#719)

--- kafka/errors.py
+++ kafka/errors.py
@@ -87,7 +87,7 @@ class InvalidMessageError(BrokerResponseError):
 
 class UnknownTopicOrPartitionError(BrokerResponseError):
     errno = 3
-    message = 'UNKNOWN_TOPIC_OR_PARTITON'
+    message = 'UNKNOWN_TOPIC_OR_PARTITION'
     description = ('This request is for a topic or partition that does not'
                    ' exist on this broker.')
     invalid_metadata = True
