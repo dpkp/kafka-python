060e8782d5ddf6e0709e829327a2dc3cc1fe3a97
Dana Powers <dana.powers@gmail.com>
2016-07-10 07:24:38 -0700

    Simplify RecordMetadata to unaltered namedtuple -- minor speed optimization

--- kafka/producer/future.py
+++ kafka/producer/future.py
@@ -38,12 +38,14 @@ class FutureRecordMetadata(Future):
         produce_future.add_errback(self.failure)
 
     def _produce_success(self, offset_and_timestamp):
-        base_offset, timestamp_ms = offset_and_timestamp
+        offset, timestamp_ms = offset_and_timestamp
         if timestamp_ms is None:
             timestamp_ms = self.timestamp_ms
-        self.success(RecordMetadata(self._produce_future.topic_partition,
-                                    base_offset, timestamp_ms,
-                                    self.relative_offset))
+        if offset != -1 and self.relative_offset is not None:
+            offset += self.relative_offset
+        tp = self._produce_future.topic_partition
+        metadata = RecordMetadata(tp[0], tp[1], tp, offset, timestamp_ms)
+        self.success(metadata)
 
     def get(self, timeout=None):
         if not self.is_done and not self._produce_future.wait(timeout):
@@ -55,18 +57,4 @@ class FutureRecordMetadata(Future):
         return self.value
 
 
-class RecordMetadata(collections.namedtuple(
-    'RecordMetadata', 'topic partition topic_partition offset timestamp')):
-    def __new__(cls, tp, base_offset, timestamp, relative_offset=None):
-        offset = base_offset
-        if relative_offset is not None and base_offset != -1:
-            offset += relative_offset
-        return super(RecordMetadata, cls).__new__(cls, tp.topic, tp.partition,
-                                                  tp, offset, timestamp)
-
-    def __str__(self):
-        return 'RecordMetadata(topic=%s, partition=%s, offset=%s)' % (
-            self.topic, self.partition, self.offset)
-
-    def __repr__(self):
-        return str(self)
+RecordMetadata = collections.namedtuple('RecordMetadata', 'topic partition topic_partition offset timestamp')
