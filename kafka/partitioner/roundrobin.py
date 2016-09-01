a88f0b9beb7ca67787a332b0b513a93ba2ea1b5a
Bar Rotstein <barrotstein@gmail.com>
2016-09-01 21:20:51 +0300

    added handling of dynamic repatitioning of kafka topics

--- kafka/partitioner/roundrobin.py
+++ kafka/partitioner/roundrobin.py
@@ -1,26 +1,63 @@
 from __future__ import absolute_import
 
-from itertools import cycle
-
 from .base import Partitioner
 
 
 class RoundRobinPartitioner(Partitioner):
-    """
-    Implements a round robin partitioner which sends data to partitions
-    in a round robin fashion
-    """
-    def __init__(self, partitions):
-        super(RoundRobinPartitioner, self).__init__(partitions)
-        self.iterpart = cycle(partitions)
+    def __init__(self, partitions=None):
+        self.partitions = None
+        self.partitions_iterable = CachedPartitionCycler(partitions)
 
-    def _set_partitions(self, partitions):
+    def _set_partitions(self, available_partitions):
+        self.partitions = available_partitions
+        self.partitions_iterable.set_partitions(available_partitions)
+
+    def __call__(self, key, all_partitions, available_partitions):
+        if available_partitions:
+            cur_partitions = available_partitions
+        else:
+            cur_partitions = all_partitions
+        if not self.partitions:
+            self._set_partitions(cur_partitions)
+        elif cur_partitions != self.partitions_iterable.partitions:
+            self._set_partitions(cur_partitions)
+        return next(self.partitions_iterable)
+
+
+class CachedPartitionCycler(object):
+    def __init__(self, partitions=None):
         self.partitions = partitions
-        self.iterpart = cycle(partitions)
+        if partitions:
+            assert type(partitions) is list
+        self.cur_pos = None
 
-    def partition(self, key, partitions=None):
-        # Refresh the partition list if necessary
-        if partitions and self.partitions != partitions:
-            self._set_partitions(partitions)
+    def __iter__(self):
+        self.next()
 
-        return next(self.iterpart)
+    @staticmethod
+    def _index_available(cur_pos, partitions):
+        return cur_pos < len(partitions)
+
+    def set_partitions(self, partitions):
+        if self.cur_pos:
+            if not self._index_available(self.cur_pos, partitions):
+                self.cur_pos = 0
+                self.partitions = partitions
+                return None
+
+            self.partitions = partitions
+            next_item = self.partitions[self.cur_pos]
+            if next_item in partitions:
+                self.cur_pos = partitions.index(next_item)
+            else:
+                self.cur_pos = 0
+            return None
+        self.partitions = partitions
+
+    def next(self):
+        if self.cur_pos is None or not self._index_available(self.cur_pos, self.partitions):
+            self.cur_pos = 1
+            return self.partitions[0]
+        cur_item = self.partitions[self.cur_pos]
+        self.cur_pos += 1
+        return cur_item
