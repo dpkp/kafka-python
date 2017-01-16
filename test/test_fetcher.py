fc1d771b5c6ad659ce8355fdc5796637505d0a4f
Jeff Widman <jeff@jeffwidman.com>
2017-01-16 13:01:01 -0800

    BROKEN: Use the new FetchRequest/Response_v3 calls
    A previous commit added the structs, but didn't actually implement
    support for using them. This adds support for actually using them

--- test/test_fetcher.py
+++ test/test_fetcher.py
@@ -60,6 +60,7 @@ def test_send_fetches(fetcher, mocker):
 
 
 @pytest.mark.parametrize(("api_version", "fetch_version"), [
+    ((0, 10, 1), 3),
     ((0, 10), 2),
     ((0, 9), 1),
     ((0, 8), 0)
