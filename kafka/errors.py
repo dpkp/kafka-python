9868ab04f70d684d1fcfd34a04a8f277d0fa37f6
Taras Voinarovskiy <voyn1991@gmail.com>
2017-07-30 12:41:27 +0000

    Make UnknownTopicOrPartitionError retriable error

--- kafka/errors.py
+++ kafka/errors.py
@@ -109,6 +109,7 @@ class UnknownTopicOrPartitionError(BrokerResponseError):
     message = 'UNKNOWN_TOPIC_OR_PARTITION'
     description = ('This request is for a topic or partition that does not'
                    ' exist on this broker.')
+    retriable = True
     invalid_metadata = True
 
 
