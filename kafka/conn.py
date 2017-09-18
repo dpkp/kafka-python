25ccc8841f6bc5fb0eb0aa2a48fdd874398d765d
dbgasaway <derekbga@gmail.com>
2017-09-18 16:10:01 -0700

    Fix not ignoring stack_info in log
    Pypy (and maybe other implementations besides CPython) doesn't ignore extra keyword arguments to log.warning, and raises an exception instead. The argument stack_info was added in 3.2, so not using it in older versions shouldn't affect the other implementations.

--- kafka/conn.py
+++ kafka/conn.py
@@ -7,6 +7,7 @@ import logging
 from random import shuffle, uniform
 import socket
 import time
+import sys
 
 from kafka.vendor import six
 
@@ -613,7 +614,10 @@ class BrokerConnection(object):
         """
         if self.state is ConnectionStates.DISCONNECTED:
             if error is not None:
-                log.warning('%s: close() called on disconnected connection with error: %s', self, error, stack_info=True)
+                if sys.version_info >= (3, 2):
+                    log.warning('%s: close() called on disconnected connection with error: %s', self, error, stack_info=True)
+                else:
+                    log.warning('%s: close() called on disconnected connection with error: %s', self, error)
             return
 
         log.info('%s: Closing connection. %s', self, error or '')
