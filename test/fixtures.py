7305f03ff0758dad811d51f5e21006f273bb4dc2
Dana Powers <dana.powers@gmail.com>
2017-10-08 10:01:03 -0700

    Update to 0.11.0.1 for travis test fixture; use as default if unspecified (#1244)

--- test/fixtures.py
+++ test/fixtures.py
@@ -21,7 +21,7 @@ log = logging.getLogger(__name__)
 
 
 class Fixture(object):
-    kafka_version = os.environ.get('KAFKA_VERSION', '0.8.0')
+    kafka_version = os.environ.get('KAFKA_VERSION', '0.11.0.1')
     scala_version = os.environ.get("SCALA_VERSION", '2.8.0')
     project_root = os.environ.get('PROJECT_ROOT', os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
     kafka_root = os.environ.get("KAFKA_ROOT", os.path.join(project_root, 'servers', kafka_version, "kafka-bin"))
