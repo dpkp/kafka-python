298709d2590b201dfe1b8753baacd1d2b554710f
Dana Powers <dana.powers@gmail.com>
2018-01-12 12:48:43 -0800

    Avoid KeyError when filtering fetchable partitions (#1344)
    * Avoid KeyError when filtering fetchable partitions

--- kafka/consumer/fetcher.py
+++ kafka/consumer/fetcher.py
@@ -638,9 +638,9 @@ class Fetcher(six.Iterator):
     def _fetchable_partitions(self):
         fetchable = self._subscriptions.fetchable_partitions()
         if self._next_partition_records:
-            fetchable.remove(self._next_partition_records.topic_partition)
+            fetchable.discard(self._next_partition_records.topic_partition)
         for fetch in self._completed_fetches:
-            fetchable.remove(fetch.topic_partition)
+            fetchable.discard(fetch.topic_partition)
         return fetchable
 
     def _create_fetch_requests(self):
