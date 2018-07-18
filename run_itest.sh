290009963933df92d7c3b1ae9108a74e2cc843fe
Manpreet Singh <manpreet@yelp.com>
2018-07-18 16:57:59 -0700

    Pin versions of packages in tox, add support for 1.0.1 itests

--- run_itest.sh
+++ run_itest.sh
@@ -1,20 +1,19 @@
 #!/bin/bash -e
 
-
-export KAFKA_VERSION='0.9.0.1'
-./build_integration.sh
-tox -e py27
-tox -e py35
-tox -e pypy
-
-export KAFKA_VERSION='0.10.0.0'
-./build_integration.sh
-tox -e py27
-tox -e py35
-tox -e pypy
-
 export KAFKA_VERSION='0.10.1.1'
 ./build_integration.sh
-tox -e py27 
-tox -e py35 
-tox -e pypy 
+tox -e py27
+tox -e py35
+tox -e pypy
+
+export KAFKA_VERSION='0.11.0.2'
+./build_integration.sh
+tox -e py27
+tox -e py35
+tox -e pypy
+
+export KAFKA_VERSION='1.0.1'
+./build_integration.sh
+tox -e py27
+tox -e py35
+tox -e pypy
