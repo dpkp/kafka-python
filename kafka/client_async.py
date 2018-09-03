0fbe6a574701dfef6b893b9d1c353850b95b70f0
Li Yazhou <me.ssword@gmail.com>
2018-09-03 15:36:20 +0800

    add wakeup_timeout_ms instead of max_block_ms in KafkaClient

--- kafka/client_async.py
+++ kafka/client_async.py
@@ -151,7 +151,7 @@ class KafkaClient(object):
         'bootstrap_servers': 'localhost',
         'client_id': 'kafka-python-' + __version__,
         'request_timeout_ms': 30000,
-        'max_block_ms': 60000,
+        'wakeup_timeout_ms': 3000,
         'connections_max_idle_ms': 9 * 60 * 1000,
         'reconnect_backoff_ms': 50,
         'reconnect_backoff_max_ms': 1000,
@@ -199,7 +199,7 @@ class KafkaClient(object):
         self._bootstrap_fails = 0
         self._wake_r, self._wake_w = socket.socketpair()
         self._wake_r.setblocking(False)
-        self._wake_w.settimeout(self.config['max_block_ms'] / 1000.0)
+        self._wake_w.settimeout(self.config['wakeup_timeout_ms'] / 1000.0)
         self._wake_lock = threading.Lock()
 
         self._lock = threading.RLock()
