8c7b7aeeef955ef6ac34636c57fa453c88ed2e7d
Dong Weicheng <dongw@yelp.com>
2018-09-07 13:59:11 -0700

    Remove tests for kafka version 0.11.0.2

--- run_itest.sh
+++ run_itest.sh
@@ -6,12 +6,6 @@ tox -e py27
 tox -e py35
 tox -e pypy
 
-export KAFKA_VERSION='0.11.0.2'
-./build_integration.sh
-tox -e py27
-tox -e py35
-tox -e pypy
-
 export KAFKA_VERSION='1.0.1'
 ./build_integration.sh
 tox -e py27
