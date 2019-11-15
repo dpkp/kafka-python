5008fdab6a9b98ffddd5963a97dbbf5c3f52d389
Tyler Lubeck <tylerl@surveymonkey.com>
2019-11-15 13:57:24 -0800

    Add ResponseType for CreatePartitionsRequest_v1

--- kafka/protocol/admin.py
+++ kafka/protocol/admin.py
@@ -853,6 +853,7 @@ class CreatePartitionsRequest_v1(Request):
     API_KEY = 37
     API_VERSION = 1
     SCHEMA = CreatePartitionsRequest_v0.SCHEMA
+    RESPONSE_TYPE = CreatePartitionsResponse_v1
 
 
 CreatePartitionsRequest = [
