12325c09baefae2396f1083bc8b037603721198c
Keith So <kso@eclipseoptions.com>
2020-11-06 16:05:55 +1100

    Only try to update sensors fetch lag if the unpacked list contains elements (#2158)
    Previously, if the `unpacked` list was empty, the call to `unpacked[-1]` would throw an `IndexError: list index out of range`

--- kafka/consumer/fetcher.py
+++ kafka/consumer/fetcher.py
@@ -817,8 +817,9 @@ class Fetcher(six.Iterator):
                               position)
                     unpacked = list(self._unpack_message_set(tp, records))
                     parsed_records = self.PartitionRecords(fetch_offset, tp, unpacked)
-                    last_offset = unpacked[-1].offset
-                    self._sensors.records_fetch_lag.record(highwater - last_offset)
+                    if unpacked:
+                        last_offset = unpacked[-1].offset
+                        self._sensors.records_fetch_lag.record(highwater - last_offset)
                     num_bytes = records.valid_bytes()
                     records_count = len(unpacked)
                 elif records.size_in_bytes() > 0:
