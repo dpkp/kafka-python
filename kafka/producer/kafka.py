7f564ae3f91943e705d9871c07e49f00193a4b32
will.k <will.k@kakaocorp.com>
2021-02-21 18:03:06 +0900

    Fix typo: veriication -> verification

--- kafka/producer/kafka.py
+++ kafka/producer/kafka.py
@@ -233,7 +233,7 @@ class KafkaProducer(object):
             should verify that the certificate matches the brokers hostname.
             default: true.
         ssl_cafile (str): optional filename of ca file to use in certificate
-            veriication. default: none.
+            verification. default: none.
         ssl_certfile (str): optional filename of file in pem format containing
             the client certificate, as well as any ca certificates needed to
             establish the certificate's authenticity. default: none.
