9ef7d6299f994fdf08abc69e776771c77676f52d
William Barnhart <williambbarnhart@gmail.com>
2023-08-02 17:26:01 -0400

    Update setup.py to have "        "Programming Language :: Python :: 2"," again

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
