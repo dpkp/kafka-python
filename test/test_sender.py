3ced4aec25f609f634acf5416f66ebf6c4fecb55
Dana Powers <dana.powers@gmail.com>
2025-04-07 15:01:48 -0700

    test_create_produce_requests

--- test/test_sender.py
+++ test/test_sender.py
@@ -1,8 +1,11 @@
 # pylint: skip-file
 from __future__ import absolute_import
 
-import pytest
+import collections
 import io
+
+import pytest
+from kafka.vendor import six
 
 from kafka.client_async import KafkaClient
 from kafka.protocol.broker_api_versions import BROKER_API_VERSIONS
@@ -20,8 +23,8 @@ def accumulator():
 
 
 @pytest.fixture
-def sender(client, accumulator, metrics, mocker):
-    return Sender(client, client.cluster, accumulator, metrics=metrics)
+def sender(client, accumulator):
+    return Sender(client, client.cluster, accumulator)
 
 
 @pytest.mark.parametrize(("api_version", "produce_version"), [
@@ -30,7 +33,7 @@ def sender(client, accumulator, metrics, mocker):
     ((0, 9), 1),
     ((0, 8, 0), 0)
 ])
-def test_produce_request(sender, mocker, api_version, produce_version):
+def test_produce_request(sender, api_version, produce_version):
     sender._client._api_versions = BROKER_API_VERSIONS[api_version]
     tp = TopicPartition('foo', 0)
     magic = KafkaProducer.max_usable_produce_magic(api_version)
@@ -40,3 +43,24 @@ def test_produce_request(sender, mocker, api_version, produce_version):
     records.close()
     produce_request = sender._produce_request(0, 0, 0, [batch])
     assert isinstance(produce_request, ProduceRequest[produce_version])
+
+
+@pytest.mark.parametrize(("api_version", "produce_version"), [
+    ((2, 1), 7),
+])
+def test_create_produce_requests(sender, api_version, produce_version):
+    sender._client._api_versions = BROKER_API_VERSIONS[api_version]
+    tp = TopicPartition('foo', 0)
+    magic = KafkaProducer.max_usable_produce_magic(api_version)
+    batches_by_node = collections.defaultdict(list)
+    for node in range(3):
+        for _ in range(5):
+            records = MemoryRecordsBuilder(
+                magic=1, compression_type=0, batch_size=100000)
+            batches_by_node[node].append(ProducerBatch(tp, records))
+            records.close()
+
+    produce_requests_by_node = sender._create_produce_requests(batches_by_node)
+    assert len(produce_requests_by_node) == 3
+    for node in range(3):
+        assert isinstance(produce_requests_by_node[node], ProduceRequest[produce_version])
