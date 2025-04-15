ea32c666db68b8a473da7cda4eec69240c9ab555
Dana Powers <dana.powers@gmail.com>
2025-04-15 14:18:56 -0700

    Move fixtures and integration tests to test/integration/

--- test/integration/fixtures.py
+++ test/integration/fixtures.py
@@ -66,7 +66,7 @@ class Fixture(object):
     kafka_version = os.environ.get('KAFKA_VERSION', '0.11.0.2')
     scala_version = os.environ.get("SCALA_VERSION", '2.8.0')
     project_root = os.environ.get('PROJECT_ROOT',
-                                  os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+                                  os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
     kafka_root = os.environ.get("KAFKA_ROOT",
                                 os.path.join(project_root, 'servers', kafka_version, "kafka-bin"))
 
