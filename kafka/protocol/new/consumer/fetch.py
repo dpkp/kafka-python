85e741eb4994fd234298c67632c5db2d2bcb0e06
Dana Powers <dana.powers@gmail.com>
2026-03-16 09:35:57 -0700

    OffsetForLeaderEpoch req/resp

--- kafka/protocol/new/consumer/fetch.py
+++ kafka/protocol/new/consumer/fetch.py
@@ -7,8 +7,12 @@ class FetchResponse(ApiMessage): pass
 class ListOffsetsRequest(ApiMessage): pass
 class ListOffsetsResponse(ApiMessage): pass
 
+class OffsetForLeaderEpochRequest(ApiMessage): pass
+class OffsetForLeaderEpochResponse(ApiMessage): pass
+
 
 __all__ = [
     'FetchRequest', 'FetchResponse',
     'ListOffsetsRequest', 'ListOffsetsResponse',
+    'OffsetForLeaderEpochRequest', 'OffsetForLeaderEpochResponse',
 ]
