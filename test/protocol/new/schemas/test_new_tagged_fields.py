8308fd37ae6790f6c7b992ed6bd87dd72405e047
Dana Powers <dana.powers@gmail.com>
2026-03-18 23:22:14 -0700

    test tagged fields with int16 + string fields

--- test/protocol/new/schemas/test_new_tagged_fields.py
+++ test/protocol/new/schemas/test_new_tagged_fields.py
@@ -8,14 +8,14 @@ from kafka.protocol.new.schemas.fields.codecs import TaggedFields, UnsignedVarIn
 
 def test_tagged_fields():
     tags = TaggedFields([
-        SimpleField({'name': 'foo', 'tag': 0, 'type': 'string', 'versions': "0+"}),
+        SimpleField({'name': 'foo', 'tag': 0, 'type': 'int16', 'versions': "0+"}),
         SimpleField({'name': 'bar', 'tag': 1, 'type': 'string', 'versions': "0+"}),
     ])
-    val = {'foo': 'fizz', 'bar': 'foobar'}
+    val = {'foo': 2, 'bar': 'foobar'}
     encoded = tags.encode(val, version=0)
-    # length(2), tag(0), size(5), len(5), 'fizz', tag(1), size(7), len(7), 'foobar'
+    # length(2), tag(0), size(2), b'\x00\x02', tag(1), size(7), len(7), 'foobar'
     expected = (UnsignedVarInt32.encode(2) +
-                UnsignedVarInt32.encode(0) + UnsignedVarInt32.encode(5) + UnsignedVarInt32.encode(5) + b'fizz' +
+                UnsignedVarInt32.encode(0) + UnsignedVarInt32.encode(2) + b'\x00\x02' +
                 UnsignedVarInt32.encode(1) + UnsignedVarInt32.encode(7) + UnsignedVarInt32.encode(7) + b'foobar')
     assert encoded == expected
     decoded = tags.decode(io.BytesIO(encoded), version=0)
