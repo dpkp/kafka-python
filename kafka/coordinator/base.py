d93a21ad22b7ce5da94688fc95032d148216870a
Dana Powers <dana.powers@gmail.com>
2026-03-19 11:37:48 -0700

    kafka.coordinator: use new attrs for group apis

--- kafka/coordinator/base.py
+++ kafka/coordinator/base.py
@@ -604,10 +604,10 @@ class BaseCoordinator(object):
                 else:
                     self._generation = Generation(response.generation_id,
                                                   response.member_id,
-                                                  response.group_protocol)
+                                                  response.protocol_name)
 
                 log.info("Successfully joined group %s %s", self.group_id, self._generation)
-                if response.leader_id == response.member_id:
+                if response.leader == response.member_id:
                     log.info("Elected group leader -- performing partition"
                              " assignments using %s", self._generation.protocol)
                     self._on_join_leader(response).chain(future)
@@ -697,10 +697,13 @@ class BaseCoordinator(object):
             Future: resolves to member assignment encoded-bytes
         """
         try:
-            members = [GroupMember(*member) if response.API_VERSION >= 5 else GroupMember(member[0], None, member[1])
+            members = [GroupMember(
+                           member_id=member[0],
+                           group_instance_id=member[1] if response.API_VERSION >= 5 else None,
+                           metadata=member[2] if response.API_VERSION >= 5 else member[1])
                        for member in response.members]
-            group_assignment = self._perform_assignment(response.leader_id,
-                                                        response.group_protocol,
+            group_assignment = self._perform_assignment(response.leader,
+                                                        response.protocol_name,
                                                         members)
         except Exception as e:
             return Future().failure(e)
@@ -747,7 +750,7 @@ class BaseCoordinator(object):
         if error_type is Errors.NoError:
             if self._sensors:
                 self._sensors.sync_latency.record((time.monotonic() - send_time) * 1000)
-            future.success(response.member_assignment)
+            future.success(response.assignment)
             return
 
         # Always rejoin on error
