b35c5a19c7669d18425c3a41e982e69a685d9fdb
Dana Powers <dana.powers@gmail.com>
2026-03-19 11:13:11 -0700

    producer tests

--- test/protocol/new/producer/test_new_produce.py
+++ test/protocol/new/producer/test_new_produce.py
@@ -5,50 +5,63 @@ from kafka.protocol.new.producer import ProduceRequest, ProduceResponse
 
 @pytest.mark.parametrize("version", range(ProduceRequest.min_version, ProduceRequest.max_version + 1))
 def test_produce_request_roundtrip(version):
-    topic_data = [
-        ProduceRequest.TopicProduceData(
-            name="topic-1",
-            partition_data=[
-                ProduceRequest.TopicProduceData.PartitionProduceData(
-                    index=0,
-                    records=b"some binary records data"
-                )
-            ]
-        )
-    ]
-    data = ProduceRequest(
-        transactional_id="trans-id" if version >= 3 else None,
+    Topic = ProduceRequest.TopicProduceData
+    Partition = Topic.PartitionProduceData
+    request = ProduceRequest(
+        transactional_id="txn-id" if version >= 3 else None,
         acks=-1,
         timeout_ms=500,
-        topic_data=topic_data
+        topic_data=[
+            Topic(
+                name="topic-1",
+                partition_data=[
+                    Partition(
+                        index=1,
+                        records=b"some binary records data"
+                    ),
+                ],
+            ),
+        ],
     )
-
-    encoded = data.encode(version=version)
+    encoded = request.encode(version=version)
     decoded = ProduceRequest.decode(encoded, version=version)
-    assert decoded == data
+    assert decoded == request
 
 
 @pytest.mark.parametrize("version", range(ProduceResponse.min_version, ProduceResponse.max_version + 1))
 def test_produce_response_roundtrip(version):
-    responses = [
-        ProduceResponse.TopicProduceResponse(
-            name="topic-1",
-            partition_responses=[
-                ProduceResponse.TopicProduceResponse.PartitionProduceResponse(
-                    index=0,
-                    error_code=0,
-                    base_offset=12345,
-                    log_append_time_ms=1000 if version >= 2 else -1,
-                    log_start_offset=100 if version >= 5 else -1
-                )
-            ]
-        )
-    ]
-    data = ProduceResponse(
-        responses=responses,
+    Topic = ProduceResponse.TopicProduceResponse
+    Partition = Topic.PartitionProduceResponse
+    BatchError = Partition.BatchIndexAndErrorMessage
+    LeaderIdAndEpoch = Partition.LeaderIdAndEpoch
+    response = ProduceResponse(
+        responses=[
+            Topic(
+                name="topic-1",
+                partition_responses=[
+                    Partition(
+                        index=1,
+                        error_code=3,
+                        base_offset=12345,
+                        log_append_time_ms=1000 if version >= 2 else -1,
+                        log_start_offset=100 if version >= 5 else -1,
+                        record_errors=[
+                            BatchError(
+                                batch_index=1,
+                                batch_index_error_message='foo',
+                            ),
+                        ] if version >= 8 else [],
+                        error_message='error' if version >= 8 else None,
+                        current_leader=LeaderIdAndEpoch(
+                            leader_id=12,
+                            leader_epoch=333,
+                        ) if version >= 10 else None,
+                    ),
+                ],
+            ),
+        ],
         throttle_time_ms=10 if version >= 1 else 0
     )
-
-    encoded = data.encode(version=version)
+    encoded = response.encode(version=version)
     decoded = ProduceResponse.decode(encoded, version=version)
-    assert decoded == data
+    assert decoded == response
