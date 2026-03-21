eb588c83d198a78f735c62796da815f1d343c099
Dana Powers <dana.powers@gmail.com>
2026-03-20 21:30:32 -0700

    Re-export submodule __all__ from kafka.protocol.new (#2756)

--- kafka/protocol/new/producer/__init__.py
+++ kafka/protocol/new/producer/__init__.py
@@ -1,12 +1,7 @@
 from .produce import *
+from .produce import __all__ as produce_all
+
 from .transaction import *
+from .transaction import __all__ as transaction_all
 
-
-__all__ = [
-    'ProduceRequest', 'ProduceResponse',
-    'InitProducerIdRequest', 'InitProducerIdResponse',
-    'AddPartitionsToTxnRequest', 'AddPartitionsToTxnResponse',
-    'AddOffsetsToTxnRequest', 'AddOffsetsToTxnResponse',
-    'EndTxnRequest', 'EndTxnResponse',
-    'TxnOffsetCommitRequest', 'TxnOffsetCommitResponse',
-]
+__all__ = produce_all + transaction_all
