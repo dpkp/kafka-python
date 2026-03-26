6c6957f991d8b685bb6933120ac2177a0ca0ed9a
Dana Powers <dana.powers@gmail.com>
2026-03-25 18:58:01 -0700

    from __future__ import annotations

--- kafka/protocol/new/consumer/metadata.py
+++ kafka/protocol/new/consumer/metadata.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from ..api_data import ApiData
 from kafka.structs import TopicPartition
 
--- kafka/protocol/new/metadata/metadata.py
+++ kafka/protocol/new/metadata/metadata.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from ..api_message import ApiMessage
 
 from kafka.util import classproperty
