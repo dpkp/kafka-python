fdfe8c23fe3df277d83f8817d354cf0cacdbc7de
Dana Powers <dana.powers@gmail.com>
2026-04-08 14:25:58 -0700

    kafka.metrics.remove_sensor lock order

--- kafka/metrics/metrics.py
+++ kafka/metrics/metrics.py
@@ -162,20 +162,20 @@ class Metrics:
         Arguments:
             name (str): The name of the sensor to be removed
         """
-        sensor = self._sensors.get(name)
-        if sensor:
-            child_sensors = None
-            with sensor._lock:
-                with self._lock:
+        with self._lock:
+            sensor = self._sensors.get(name)
+            if sensor:
+                child_sensors = None
+                with sensor._lock:
                     val = self._sensors.pop(name, None)
                     if val and val == sensor:
                         for metric in sensor.metrics:
                             self.remove_metric(metric.metric_name)
                         logger.debug('Removed sensor with name %s', name)
                         child_sensors = self._children_sensors.pop(sensor, None)
-            if child_sensors:
-                for child_sensor in child_sensors:
-                    self.remove_sensor(child_sensor.name)
+                if child_sensors:
+                    for child_sensor in child_sensors:
+                        self.remove_sensor(child_sensor.name)
 
     def add_metric(self, metric_name, measurable, config=None):
         """
