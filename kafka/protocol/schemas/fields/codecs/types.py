6568f58703b05d46b85af978ec7108ac0ace30f4
Dana Powers <dana.powers@gmail.com>
2026-04-09 22:18:08 -0700

    Bytes accepts bytearray/memoryview

--- kafka/protocol/schemas/fields/codecs/types.py
+++ kafka/protocol/schemas/fields/codecs/types.py
@@ -219,19 +219,19 @@ class Bytes:
 
     @classmethod
     def encode(cls, value, compact=False):
-        if value is not None and not isinstance(value, bytes):
+        if value is not None and not isinstance(value, (bytes, bytearray, memoryview)):
             value = value.encode()
         if compact:
             if value is None:
                 return UnsignedVarInt32.encode(0)
-            return UnsignedVarInt32.encode(len(value) + 1) + value
+            return UnsignedVarInt32.encode(len(value) + 1) + bytes(value)
         if value is None:
             return Int32.encode(-1)
-        return Int32.encode(len(value)) + value
+        return Int32.encode(len(value)) + bytes(value)
 
     @classmethod
     def encode_into(cls, out, value, compact=False):
-        if value is not None and not isinstance(value, bytes):
+        if value is not None and not isinstance(value, (bytes, bytearray, memoryview)):
             value = value.encode()
         if compact:
             if value is None:
@@ -257,7 +257,7 @@ class Bytes:
         bn = ctx.next_var('bn')
         if compact:
             ctx.emit(indent, '%s = %s' % (bv, val_expr))
-            ctx.emit(indent, 'if %s is not None and not isinstance(%s, bytes): %s = %s.encode()' % (bv, bv, bv, bv))
+            ctx.emit(indent, 'if %s is not None and not isinstance(%s, (bytes, bytearray, memoryview)): %s = %s.encode()' % (bv, bv, bv, bv))
             ctx.emit(indent, 'if %s is None:' % bv)
             ctx.emit(indent, '    buf[pos] = 0')
             ctx.emit(indent, '    pos += 1')
@@ -273,7 +273,7 @@ class Bytes:
             ctx.emit(indent, '    pos += %s' % bn)
         else:
             ctx.emit(indent, '%s = %s' % (bv, val_expr))
-            ctx.emit(indent, 'if %s is not None and not isinstance(%s, bytes): %s = %s.encode()' % (bv, bv, bv, bv))
+            ctx.emit(indent, 'if %s is not None and not isinstance(%s, (bytes, bytearray, memoryview)): %s = %s.encode()' % (bv, bv, bv, bv))
             ctx.emit(indent, 'if %s is None:' % bv)
             ctx.emit(indent, "    pack_into('>i', buf, pos, -1)")
             ctx.emit(indent, '    pos += 4')
