db101f91c640ecd20425f8fd021579e9983e3ec5
Dana Powers <dana.powers@gmail.com>
2026-04-22 09:19:09 -0700

    Admin: support incremental alter configs

--- kafka/admin/__init__.py
+++ kafka/admin/__init__.py
@@ -1,8 +1,9 @@
 from kafka.admin.client import KafkaAdminClient
-from kafka.admin._acls import (ACL, ACLFilter, ResourcePattern, ResourcePatternFilter, ACLOperation,
-                                ResourceType, ACLPermissionType, ACLResourcePatternType)
+from kafka.admin._acls import (
+    ACL, ACLFilter, ResourcePattern, ResourcePatternFilter, ACLOperation,
+    ResourceType, ACLPermissionType, ACLResourcePatternType)
 from kafka.admin._configs import (
-    ConfigResource, ConfigResourceType, ConfigType, ConfigSourceType)
+    AlterConfigOp, ConfigResource, ConfigResourceType, ConfigType, ConfigSourceType)
 from kafka.admin._groups import MemberToRemove
 from kafka.admin._partitions import NewPartitions, OffsetSpec
 from kafka.admin._topics import NewTopic
@@ -13,7 +14,7 @@ __all__ = [
     'KafkaAdminClient',
     'ACL', 'ACLFilter', 'ACLOperation', 'ACLPermissionType', 'ACLResourcePatternType',
     'ResourceType', 'ResourcePattern', 'ResourcePatternFilter',
-    'ConfigResource', 'ConfigResourceType', 'ConfigType', 'ConfigSourceType',
+    'AlterConfigOp', 'ConfigResource', 'ConfigResourceType', 'ConfigType', 'ConfigSourceType',
     'MemberToRemove', 'OffsetSpec', # NewTopic + NewPartitions are deprecated and not included in __all__
     'ScramMechanism', 'UserScramCredentialDeletion', 'UserScramCredentialUpsertion',
 ]
