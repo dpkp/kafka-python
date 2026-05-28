80e52bec3328c929222aa20339e569991ed7fe2c
Noa Levi <275430404+lphuc2250gma@users.noreply.github.com>
2026-05-28 04:10:35 +0000

    chore: improve kafka-python maintenance path

--- test/test_util.py
+++ test/test_util.py
@@ -7,6 +7,7 @@ from kafka.util import ensure_valid_topic_name
 @pytest.mark.parametrize(('topic_name', 'expectation'), [
     (0, pytest.raises(TypeError)),
     (None, pytest.raises(TypeError)),
+    (b'bytes-topic', pytest.raises(TypeError)),
     ('', pytest.raises(ValueError)),
     ('.', pytest.raises(ValueError)),
     ('..', pytest.raises(ValueError)),
@@ -22,3 +23,15 @@ def test_topic_name_validation(topic_name, expectation):
     with expectation:
         ensure_valid_topic_name(topic_name)
 
+
+@pytest.mark.parametrize('topic_name', [
+    'a',
+    'topic',
+    'my-topic',
+    'my_topic',
+    'my.topic',
+    'topic123',
+    'a' * 249,
+])
+def test_topic_name_validation_success(topic_name):
+    assert ensure_valid_topic_name(topic_name) is None
