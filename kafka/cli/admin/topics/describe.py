af4dbfce9f5bdea3f1345b49ac54fb74762e3085
Dana Powers <dana.powers@gmail.com>
2026-06-05 09:38:40 -0700

    Add --id support to admin cli describe-topics

--- kafka/cli/admin/topics/describe.py
+++ kafka/cli/admin/topics/describe.py
@@ -1,11 +1,18 @@
+import uuid
+
+
 class DescribeTopics:
     COMMAND = 'describe'
     HELP = 'Describe Kafka Topics'
 
     @classmethod
     def add_arguments(cls, parser):
-        parser.add_argument('-t', '--topic', type=str, action='append', dest='topics')
+        parser.add_argument('-t', '--topic', type=str, action='append', dest='topics', default=[], help='topic name')
+        parser.add_argument('--id', type=str, action='append', dest='topic_ids', default=[],
+                            help='topic UUID (requires broker >= 2.8, KIP-516)')
 
     @classmethod
     def command(cls, client, args):
-        return client.describe_topics(args.topics or None)
+        topic_ids = [uuid.UUID(topic_id) for topic_id in args.topic_ids]
+        topics = args.topics + topic_ids
+        return client.describe_topics(topics or None)
